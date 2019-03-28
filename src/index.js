8882cd1ed2941b7d4cc0bb04c6ddc742c138d8ba
Phumrapee Limpianchop <git@rayriffy.com>
2019-03-28 14:18:32 +0700

    :sparkles: Functional plugin

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash'
+import visit from 'async-unist-util-visit'
+
+const ENDPOINT = 'https://embed.music.apple.com/'
+
+export default async ({markdownAST}, options) => {
+  const res = await visit(markdownAST, 'inlineCode', async node => {
+    if (!node.value.startsWith('itunes:')) return
+
+    const baseUrl = node.value.substring(7)
+
+    const embedUrl = baseUrl.replace('https://itunes.apple.com/', ENDPOINT)
+
+    let height = 450
+
+    if (!_.isEmpty(baseUrl.split('i=')[1])) height = 150
+
+    node.type = 'html'
+    node.value = `<iframe src="${embedUrl}" style="border: 0; width: 100%; height: ${height};" allowtransparency="true" allow="encrypted-media"></iframe>`
+
+    return markdownAST
+  })
+
+  return res
+}
